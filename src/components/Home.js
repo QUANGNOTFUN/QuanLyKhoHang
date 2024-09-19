9f31a9951978bd745c6462512c4fbd24080c78fe
Phạm Đăng Quang <140483907+QUANGNOTFUN@users.noreply.github.com>
2024-09-19 23:51:42 +0700

    thêm folder lưu các page con

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Trang Chủ</h1>
+            <p>Chào mừng đến với quản lý kho hàng!</p>
+        </div>
+    );
+}
+
+export default Home;
